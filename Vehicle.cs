1d628b7abdcd0d469ae96c9b6331d8709e3de6c0
MIGUEL  ESTEVAO TIEZI DE OLIVEIRA <23053@fatectq.local>
2024-03-20 08:43:45 -0300

    first commit

---
+++ Vehicle.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMS2A.ClassObject
+{
+    ///<summary>
+    /// <author>Miguel</author> 
+    ///</summary>
+
+    public abstract class Vehicle
+    {
+        public int Id {get; set;}
+        public int Year {get; set;}
+        public string? Modelo {get; set;}
+        public string? color {get; set;}
+        public string? Brand {get; set;}
+
+
+
+       
+
+        }
+    }
